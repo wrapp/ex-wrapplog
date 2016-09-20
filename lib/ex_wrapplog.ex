12acaf2a2f2f97595a97f17ae1fcb6cda530a7cf
Zeeshan Abid <zeeshan.abid@wrapp.com>
2016-09-20 10:38:31 +0200

    Some docs

--- lib/ex_wrapplog.ex
+++ lib/ex_wrapplog.ex
@@ -3,9 +3,8 @@ defmodule ExWrapplog do
   @levels [{:debug, 0}, {:info, 1}, {:warn, 2}, {:error, 3}]
 
   @doc """
-  Generates a JSON log and writes to the standard ourput
+  Generates a JSON log and writes to the standard output.
   """
-
   def log(level, msg, %{} = params \\ %{}) do
     if should_log?(level) do
       log_event(level, msg, params)
@@ -15,9 +14,24 @@ defmodule ExWrapplog do
     end
   end
 
+  @doc """
+  Logs the message at debug level to the standard output
+  """
   def debug(msg, params \\ %{}), do: :debug |> log(msg, params)
+
+  @doc """
+  Logs the message at info level to the standard output
+  """
   def info(msg, params  \\ %{}), do: :info  |> log(msg, params)
+
+  @doc """
+  Logs the message at warn level to the standard output
+  """
   def warn(msg, params  \\ %{}), do: :warn  |> log(msg, params)
+
+  @doc """
+  Logs the message at error level to the standard output
+  """
   def error(msg, params \\ %{}), do: :error |> log(msg, params)
 
   defp log_event(level, msg, %{} = params) do
